e08011604615c6782072a03e6364fd36f371587a
hiroki ohno <h_ohno@interspace.ne.jp>
2018-10-25 17:59:55 +0900

    TypBoardの簡易的な実装

--- src/components/TypBoard.vue
+++ src/components/TypBoard.vue
@@ -1,11 +1,47 @@
 <template>
 <div>
-    This is TypeBoard
+    <span v-for="(c, index) in codeArry"
+    v-bind:key="index"
+    v-bind:class="{done: typindex > index}">
+    {{  c }}
+    <br v-if="c==='\n'">
+    </span>
 </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      orinalcode: `this is Sample`,
+      typindex: 0
+    };
+  },
+  created() {
+    window.addEventListener("keydown", this.typHandler);
+  },
+  computed: {
+    codeArry() {
+      return this.orinalcode.split("");
+    }
+  },
+  methods: {
+    typHandler(event) {
+      const c1 = this.codeArry[this.typindex];
+      if (event.key === c1) {
+        this.typindex++;
+      }
+    }
+  }
+};
+</script>
 
 <style scoped>
 div {
   background-color: white;
+}
+
+.done {
+  color: red;
 }
 </style>
